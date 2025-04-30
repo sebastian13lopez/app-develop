be886c24d071931a1d0ac3cd94c4b90be92de3df
sebastian13lopez <jslopezr@ufpso.edu.co>
2025-04-29 20:28:47 -0500

    Se agrego dashboard de admin

--- js/loginService.js
+++ js/loginService.js
@@ -2,28 +2,61 @@ document.getElementById('loginForm').addEventListener('submit',function(e){
     e.preventDefault();
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value;
-    let mensaje = '';
-    let tipoAlerta = '';
-
-    if(email === '' || password === ''){
-        mensaje = 'Por favor completa todos los campos'
-        tipoAlerta='warning'
-    }
-
-    else if(email === 'prueba@gmail.com' && password === '123456'){
-        mensaje = 'Inicio de sesion exitoso'
-        tipoAlerta='success'
-    }
-
-    else{
-        mensaje = 'Correo o contraseña incorrectos.'     
-        tipoAlerta='danger'  
-        }
-
-    let alerta = 
-    `<div class="alert alert-${tipoAlerta} alert-dismissible fade show" role="alert">
-    ${mensaje}  
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>`
-  document.getElementById('mensaje').innerHTML = alerta;
+    login(email, password)
 }) 
+
+
+function login(email, password){
+    localStorage.removeItem('token')
+    let message = ''
+    let alertType = ''
+    const REQRES_ENDPOINT = 'https://reqres.in/api/login'
+    fetch(REQRES_ENDPOINT,{
+        method:'POST',
+        headers:{ 
+            'Content-type': 'application/json',
+            'x-api-key': 'reqres-free-v1'
+        },
+        body: JSON.stringify({email, password})
+    })
+
+    .then((response) => {
+        if(response.status === 200){
+        alertType = 'success'
+        message = 'Inicio de sesion exitoso'
+        alertBuilder(alertType, message)
+        localStorage.setItem('token', "marcosesgay")
+        setTimeout(() => {
+            location.href = 'admin/dashboard.html'
+            
+        }, 2000) //2000 es = 2 segundos
+        
+    }
+        else{
+            alertType = 'danger'
+            message = 'Correo o contraseña invalida'  
+            alertBuilder(alertType, message)      
+        }        
+        console.log('respuesta del servicio', response)
+        
+    })
+
+    
+
+    .catch((error) => {
+        alertType = 'danger'
+        message = 'Correo o contraseña invalida'
+        console.log('respuesta del servicio', error)
+        alertBuilder(alertType, message)
+    })
+}
+
+function alertBuilder(alertType, message){
+    const alert = 
+         `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+         ${message}  
+         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`
+  document.getElementById('mensaje').innerHTML = alert;
+
+}
